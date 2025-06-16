8d7d151ec7f2c22cbf395e9db26e96e6ba206002
Lucas Henrique <lucas.henrique.fm99@gmail.com>
2025-06-16 18:34:56 -0300

    teste_k6_grafana_3

--- scripts/teste.js
+++ scripts/teste.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check, group, sleep } from 'k6';
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
-import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+// import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+// import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 export let options = {
   vus: 30,
@@ -21,9 +21,9 @@ export default function () {
   })
 }
 
-export function handleSummary(data) {
-  return {
-    'relatorio.html': htmlReport(data),
-    stdout: textSummary(data, { indent: ' ', enableColors: true }),
-  };
-}
+// export function handleSummary(data) {
+//   return {
+//     'relatorio.html': htmlReport(data),
+//     stdout: textSummary(data, { indent: ' ', enableColors: true }),
+//   };
+// }
